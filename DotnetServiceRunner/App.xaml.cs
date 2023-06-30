9cc1e3729fbe83c73981218c12a5bb7e1daa461f
Matthew <16mdlewis@gmail.com>
2023-06-30 22:57:06 +0200

    Initial

---
+++ DotnetServiceRunner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetServiceRunner;
+
+public partial class App : Application
+{
+  public App()
+  {
+    InitializeComponent();
+
+    MainPage = new AppShell();
+  }
+}
---
+++ DotnetServiceRunner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetServiceRunner;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
